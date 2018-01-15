69d751fc0b34c479a69b5e489ae75f48b23e71c4
Jan Blatnik <jb4634@student.uni-lj.si>
2018-01-15 01:14:32 +0100

    Preverim ce je revert uspel

--- src/main/java/com/fri/musicbook/ArtistSubscriptionsHealthCheckBean.java
+++ src/main/java/com/fri/musicbook/ArtistSubscriptionsHealthCheckBean.java
@@ -1,12 +1,10 @@
 package com.fri.musicbook;
 
-import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -14,15 +12,17 @@ import java.net.URL;
 @ApplicationScoped
 public class ArtistSubscriptionsHealthCheckBean implements HealthCheck{
 
+    /*
     @Inject
-    @DiscoverService("artist-service")
-    private String basePath;
-    private static final String url = "http://169.51.16.117:30893/v1/artistSubscription";
+    @DiscoverService("artists")
+    private Optional<String> url;
+    */
+    private static final String url = "http://localhost:8085/v1/artistSubscription";
 
     @Override
     public HealthCheckResponse call() {
         try {
-            System.out.println("kajeto:"+basePath);
+
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setRequestMethod("HEAD");
 
